c504f7caa7dd6c3da4918d89f2f9d3e5d0ee746e
eslinmakicine <eslinmartins@gmail.com>
2019-11-13 18:43:38 -0300

    Tela de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-@Component({
+@Component({  
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
   constructor(private router: Router) { }
+  emailField = new FormControl('', [Validators.required]);
+  passwordField = new FormControl('', [Validators.required]);
 
+
+  public logar_click() {
+  }
   ngOnInit() {
     // this.router.navigate([])
   }
